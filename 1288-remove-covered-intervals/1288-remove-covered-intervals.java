baedac6ec57d4e0fae2d29d66eb2014d6af6d563
Palak Agrawal <palakagrawal065478@gmail.com>
2026-07-06 23:57:12 +0530

    Time: 6 ms (93.21%), Space: 46.5 MB (43.52%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals , (a,b)->{
+        if(a[0]==b[0]) return b[1]-a[1];
+        return a[0]-b[0];});
+        int max_end=0;
+        int count=0;
+        for(int[] interval:intervals){
+            if(interval[1]>max_end){
+                max_end=interval[1];
+                count++;
+            }
+
+        }
+        return count;
+    }
+}
